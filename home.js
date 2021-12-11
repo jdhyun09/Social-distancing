702f09d910340eeacae635a39226697ae97de9c0
jdhyun09 <jdhyun09@naver.com>
2021-12-11 17:21:56 +0900

    update

---
+++ home.js
@@ -0,0 +1,14 @@
+    // var metropolitan = {
+    //     현재 사회적 거리두기 단계 : ,
+    //     운영시간 : ,
+    //     인원 제한 : ,
+    //     이용가능대상 : ,
+    //     방역패스 : ,
+    // }
+
+    function info_show(){
+        document.getElementById("informationSpace").style.display = "block";
+    }
+    function info_hide(){
+        document.getElementById("informationSpace").style.display = "none";
+    }
